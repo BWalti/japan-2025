5ead63af38d3f0a9e11be01e3112c51e11db01af
Beat Walti <beat_walti@epam.com>
2025-05-04 21:30:40 +0900

    feat: trying to get images to work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,54 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  modules: [
-      '@nuxt/content',
-    '@nuxt/icon',
-    '@nuxt/eslint',
-    '@nuxtjs/color-mode',
-    '@nuxt/image',
-    'nuxt-maplibre'],
+    modules: [
+        '@nuxt/content',
+        '@nuxt/icon',
+        '@nuxt/eslint',
+        '@nuxtjs/color-mode',
+        '@nuxt/image',
+        'nuxt-maplibre'],
 
-  runtimeConfig: {
-    public: {
-      weatherApiKey: 'f0aac73bbe63716305955fcb468aac77', // hier leer lassen, füllst du über .env
-      appUrl: 'https://jp25.bwalti.net'
-    }
-  },
-
-  ssr: true,
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: ["/rss.xml"],
+    runtimeConfig: {
+        public: {
+            weatherApiKey: 'f0aac73bbe63716305955fcb468aac77', // hier leer lassen, füllst du über .env
+            appUrl: 'https://jp25.bwalti.net'
+        }
     },
-  },
 
-  css: ['assets/css/main.css'],
-  colorMode: { classSuffix: '' },
-
-  components: true,
-  content: {
-    build: {
-      markdown: {
-        highlight: {
-          // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
-          theme: {
-            dark: 'github-dark',
-            default: 'github-light',
-          },
+    ssr: true,
+    nitro: {
+        prerender: {
+            crawlLinks: true,
+            routes: ["/rss.xml"],
         },
-      },
     },
-  },
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: '2025-02-19',
 
-  vite: { plugins: [tailwindcss()] },
+    routeRules:{
+        '/_ipx/**': { prerender: true },
+        '/**': { prerender: true }
+    },
+    css: ['assets/css/main.css'],
+    colorMode: {classSuffix: ''},
+
+    components: true,
+    content: {
+        build: {
+            markdown: {
+                highlight: {
+                    // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+                    theme: {
+                        dark: 'github-dark',
+                        default: 'github-light',
+                    },
+                },
+            },
+        },
+    },
+    future: {
+        compatibilityVersion: 4,
+    },
+    compatibilityDate: '2025-02-19',
+
+    vite: {plugins: [tailwindcss()]},
 })
